52bd4e217b35c2cd56e6abcd1a4cb34cf7534569
Cecilia Cid <ccid.montero@gmail.com>
2019-02-12 11:42:01 -0300

    subiendo actualizaciones, y adjunto sensuales imagenes del gatoto con lentes

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,37 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+//función que codifica mayusculas.
+function encode (numb,text) {
+  let resultado="";
+  let ascii=0;
+  //2) crear un loop.
+  for(let i=0; i< text.length; i++){
+  //3)aplicar codigo ascii.
+    ascii = text.charCodeAt(i);
+  //4)aplicar formula offset.
+    if(ascii>=65 && ascii<=90){
+    ascii=(ascii-65+parseInt(numb))%26+65;
+    }
+    if(ascii>=97 && ascii<=122){
+      ascii=(ascii-97+parseInt(numb))%26+97;
+    }
+  //5)tomar ascii y usar. 
+    resultado+=String.fromCharCode(ascii)
   }
-};
+  return resultado;
+}
+
+//Función que decondifica minusculas.
+function decode (numb,text) {
+  let resultado = "";
+  let ascii = 0;
+  for (let i=0; i<text.length;i++){
+   ascii=text.charCodeAt(i);
+    if(ascii>=65 && ascii<=90){
+      ascii = (ascii-39-parseInt(numb))%26+65;
+    }
+    if(ascii>=97 && ascii<=122){
+      ascii = (ascii-71-parseInt(numb))%26+97;
+    }
+      resultado += String.fromCharCode(ascii);
+  }
+  return resultado;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-/* Acá va tu código */
+//Función para pasar de pantalla 1 a 2.
+function changeScreen(){
+  document.getElementById("screen2").style.display="block";
+  document.getElementById("screen1").style.display="none";
+ //Cambia el texto para saludar al usuario por su nombre.
+  let aText = document.getElementById("greeting");
+  let name = document.getElementById("userName").value;
+  aText.innerHTML="Hola " + name +" comienza por elegir un número de desplazamiento y escribe lo que quieras."; 
+  //primera linea de la pantalla.
+document.documentElement.scrollTop=0;
+  //Limpia el campo.
+  document.getElementById("userName").value="";
+}
+//Función para pasar de pantalla 2 a 3 + saludo
+function changeScreen2(){
+  document.getElementById("screen1").style.display="block";
+  document.getElementById("screen2").style.display="none";
+    //primera linea de la pantalla.
+    document.documentElement.scrollTop=0;
+}
+//tomando valores
+function takeValue(){
+  let number=parseInt(document.getElementById("numbBox").value);
+  let text=document.getElementById("text1").value;
+  if (document.getElementById("encode").checked){
+     document.getElementById("text2").value=encode(number,text);
+  }
+  else{
+      document.getElementById("text2").value=decode(number,text);
+  }
+  document.getElementById("text1").value="";
+}
+//Enviar el correo (en realidad sólo es limpiar el campo).
+function sendMail() {
+let eMail = document.getElementById("eMail").value;
+document.getElementById("eMail").value="";
+}
