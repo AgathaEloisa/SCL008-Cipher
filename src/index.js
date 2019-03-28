cc8d5fa5817ba3d14ae65f079fe42c0f4d9d7c76
AgathaP <ebrumaktub@gmx.com>
2019-03-27 22:40:46 -0300

    cambios en archivos js, css, html, uso correcto del DOM, nueva funcionalidad en boton btnconvert

--- src/index.js
+++ src/index.js
@@ -1,120 +1,125 @@
-//DOM!!
-function home () {
-  document.getElementById ("screen") .innerHTML = " ";
-  document.getElementById ("screen") .innerHTML += 
-`
-    <!-- Pantalla home dividida en part1 y part2 respectivamente. -->
-<section id = "part1">
+// Manejo del DOM
+
+//-------------- Pantalla uno ------------//
+
+document.getElementById('screen').innerHTML = '';
+let home = document.getElementById('screen').innerHTML += 
+    
+`        
+<div id="part1">
     <!--funcionamiento y linkeo a información adicional en wikipedia-->
-  <div>
     <h2> Cómo funciona: </h2>
     <p> Esta app usa el metodo de 
-      <a href = "https://es.wikipedia.org/wiki/Cifrado_César" target=”_blank”> cifrado cesar </a> . <br>
+    <a href="https://es.wikipedia.org/wiki/Cifrado_César" target=”_blank”> cifrado cesar </a> . <br>
 Una vez que escribas tu mensaje en la caja de texto, puedes cifrarlo o descifrarlo según sea el caso, y enviarlo a cualquier dirección de correo electrónico. <br>
 Lo que escribas no será archivado. Comparte secretos con tus amigos, organicen fiestas, paseos, etc. tu decides como usarla. </p> </div>
-  </div>
-</section>
-<section id = "part2">
-    <!--interacción con el usuario, petición de nombre-->
-  <div>
-    <h1>¿cuál es tu nombre?</h1>
-    <input type="text" id="userName" placeHolder="tu nombre aquí">
-    <input type="submit" id="btnEnter" value="Entrar">
-  </div>
-</section>
+</div>
+<div id="part2">
+<!--interacción con el usuario, petición de nombre-->
+  <h1>¿cuál es tu nombre?</h1>
+  <input type="text" id="username" placeHolder="tu nombre aquí">
+  <input type="submit" id="btnenter" value="Entrar">
+</div>
+${document.documentElement.scrollTop=0}
 `
-};
+
+document.getElementById('username').value='';
 
 
-  //Función para pasar de pantalla 1 a 2.
-const changeScreen = document.getElementById ("btnEnter") .addEventListener ("click", () => {
-document.getElementById ("screen") .innerHTML = " ";
-document.getElementById ("screen") .innerHTML += 
-`
-    <!--Pantalla n°2-->
-    <!--Saludo personalizado con el nombre del usuario-->
-  <div>
-    <h2 id="greeting"></h2>
-  </div>
-  <div>
+//-------------- Pantalla dos ------------//
+
+// mostrar pantalla 2
+document.getElementById('btnenter').addEventListener('click', () => {
+    let name = document.getElementById('username').value;
+    document.getElementById('screen').innerHTML = '';
+    document.getElementById('screen').innerHTML += 
+    
+    `
+    <h2 id="greeting"> Hola ${name} comienza por elegir un número de 
+    desplazamiento y escribe lo que quieras </h2>
+
     <h2> N° de desplazamiento:  
-      <input type="number" min="1" max="99" placeholder="N°" id="numbBox" value="0">
+      <input type="number" min="1" max="99" placeholder="N°" id="numbbox" value="0">
     </h2>
-  </div>
-  <div class="inpRadio"> 
+
+    <div class="inpradio"> 
       <h3>
         <input id="encode" type="radio" name="change" value="Codificar" checked>Codificar
       </h3>
       <h3>
         <input id="decode" type="radio" name="change" value="Decodificar">Decodificar
       </h3>
-  </div>
-    <!--Donde el usuario escribe el mensaje a decodificar-->
-    <input type="text" id="text1">
+    </div>
+
+    </div>
+        <!--Donde el usuario escribe el mensaje a decodificar-->
+        <input type="text" id="text1">
+    <div>
+
     <!--Botón convertir, que codifica el texto que entrega el usuario-->
-  <div>
-      <input type="button" id="btnConvert" value="Convertir" onclick="takeValue()">
-  </div>
-    <!--Donde el usuario recibe su mensaje procesado-->
-  <div>
-  <h3>Resultado:</h3>
-    <input type="text" id="text2">
-  </div>
-    <!--caja de texto donde va el correo-->
-  <div>
-      <input type="email" id="eMail" placeHolder="Ingresa correo"><input type="submit" id="btnSend" value="Enviar" onclick="sendMail()">
-  </div>
-  <div> 
-    <!--Botón que envía a pag.1-->
-      <input type="button" id="btnhome" value= "cambiar de usuario">
-  </div>
-`
+      <input type="submit" id="btnconvert" value="Convertir">
+
+    <div>
+      <h2 class="textresult">Resultado:</h2>
+        <input type="text" id="text2">
+    </div>
+
+        <!--caja de texto donde va el correo-->
+    <div>
+        <input type="email" id="email" placeHolder="Ingresa correo"><input type="submit" id="btnsend" value="Enviar">
+    </div>
+
+        <!--Botón que envía a pag.1-->
+        <input type="submit" id="btnhome" value= "cambiar de usuario">
+    `
+
+
+    // Función que codifica y decodifica seún el radio button seleccionado.
+document.getElementById('btnconvert').addEventListener('click', () => {
+    let number = parseInt(document.getElementById('numbbox').value);
+    let text = document.getElementById('text1').value;
+
+      if(number === 0){
+        // modal first attempt
+        alert('por favor selecciona un número de desplazamiento');
+
+
+      } else {
+        if (document.getElementById('encode').checked) {
+          document.getElementById('text2').value = encode(number,text);
+        }
+        else{
+          document.getElementById('text2').value = decode(number,text);
+        }
+      };
+
+    document.getElementById('text1').value = '';
+    document.documentElement.scrollTop=0;
 });
 
-  //Cambia el texto para saludar al usuario por su nombre.
-const userGreeying = document.getElementById ("greeting") .addEventListener ("click", () => {
-let aText = document.getElementById("greeting");
-let name = document.getElementById("userName").value;
-aText.innerHTML=`<h2> Hola ${name} comienza por elegir un número de desplazamiento y escribe lo que quieras </h2>` 
-  //primera linea de la pantalla.
-document.documentElement.scrollTop=0;
-  //Limpia el campo.
-document.getElementById("userName").value="";
-});
-
-
-const returnHome = document.getElementById ("btnhome") .addEventListener ("click", () => {
-    document.getElementById ("screen") .innerHTML = " ";
-    document.getElementById ("screen") .innerHTML += `${home}`;
-})
-//document.getElementById ("btnhome") .addEventListener("click", () => {
-
-  //hay que darle la funcionalidad al botón "btnEnter"
-  //document.getElementById("screen2").style.display="block";
-  //document.getElementById("screen1").style.display="none";
-
-
-//Función para pasar de pantalla 2 a 3 + saludo
-//function changeScreen2(){
- // document.getElementById("screen1").style.display="block";
-//  document.getElementById("screen2").style.display="none";
-    //primera linea de la pantalla.
-  //  document.documentElement.scrollTop=0;
-//}
-//tomando valores
+ /*   //tomando valores
 function takeValue(){
-  let number=parseInt(document.getElementById("numbBox").value);
-  let text=document.getElementById("text1").value;
-  if (document.getElementById("encode").checked){
-     document.getElementById("text2").value=encode(number,text);
-  }
-  else{
-      document.getElementById("text2").value=decode(number,text);
-  }
-  document.getElementById("text1").value="";
-}
-//Enviar el correo (en realidad sólo es limpiar el campo).
-function sendMail() {
-let eMail = document.getElementById("eMail").value;
-document.getElementById("eMail").value="";
-}
+    let number=parseInt(document.getElementById("numbBox").value);
+    let text=document.getElementById("text1").value;
+    if (document.getElementById("encode").checked){
+       document.getElementById("text2").value=encode(number,text);
+    }
+    else{
+        document.getElementById("text2").value=decode(number,text);
+    }
+    document.getElementById("text1").value="";
+  };
+*/
+    document.documentElement.scrollTop=0;
+});
+
+document.getElementById('btnhome').addEventListener('click', () => {
+    document.getElementById('screen').innerHTML = '';
+    document.getElementById('screen').innerHTML += 
+    
+    `
+    ${home}
+    `
+
+    document.documentElement.scrollTop=0;
+});
